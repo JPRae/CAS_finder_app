3ef6a5b58da85607581acf8677642e5afc58aab8
JPRae <80130806+JPRae@users.noreply.github.com>
2021-11-25 23:04:59 +0100

    Add files via upload

---
+++ CAS_Searcher_App.py
@@ -0,0 +1,58 @@
+from CAS_Searcher import CAS_Searcher
+import streamlit as st
+import streamlit_analytics
+import streamlit.components.v1 as components
+
+# search_term = "66728-98-1"
+# search_term = "497-23-4" # furanone test
+#search_term = "12135-22-7" # Pd/C
+#search_term = "74-88-4" # iodomethane 
+#search_term = "45-125-15478" #Not valid CAS
+#search_term = "22122-36-7"
+#search_term = "100-52-7" # benzaldehyde 
+# search_term = "51673-83-7" # Building block explorer
+#CAS_Searcher(search_term)   
+
+with streamlit_analytics.track():
+    st.set_page_config(layout="wide")
+
+    st.write("""# CAS Searcher""")
+
+    with st.form(key='searchform'):
+        nav1,nav2 = st.columns([1,1])
+
+        with nav1:
+            search_term = st.text_input("Enter CAS Number  eg 497-23-4", help = "eg 497-23-4", type = "default")
+
+        with nav2:
+            st.text("Search")
+            submit_search = st.form_submit_button(label='Search')
+
+    col1, col2 = st.columns([2,1])
+
+    # streamlit_analytics.start_tracking()
+    with col1:
+        if submit_search:
+
+            results = CAS_Searcher(search_term)
+
+    # streamlit_analytics.stop_tracking()
+
+#analytics code:
+components.html(
+"""
+<!-- Default Statcounter code for CAS Finder
+https://share.streamlit.io/jprae/cas_finder_app/main/CAS_Searcher_App.py -->
+<script type="text/javascript">
+var sc_project=12682478; 
+var sc_invisible=1; 
+var sc_security="74f5936b"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="website statistics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12682478/0/74f5936b/1/" alt="website statistics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
+""")
